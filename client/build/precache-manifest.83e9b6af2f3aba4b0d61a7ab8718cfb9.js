e5df1bfde8e0fb0aedd38870b47ba4ab9b5e38ea
gerardDevelop <gerard.develop@gmail.com>
2019-04-01 15:46:58 +0700

    dsad

---
+++ client/build/precache-manifest.83e9b6af2f3aba4b0d61a7ab8718cfb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ae7c3d160314e340fca4",
+    "url": "/static/js/main.33874156.chunk.js"
+  },
+  {
+    "revision": "fb2354e316dfe268acf1",
+    "url": "/static/js/2.bc393022.chunk.js"
+  },
+  {
+    "revision": "ae7c3d160314e340fca4",
+    "url": "/static/css/main.91383797.chunk.css"
+  },
+  {
+    "revision": "ba51acc6290997f56d835484e365185f",
+    "url": "/index.html"
+  }
+];
