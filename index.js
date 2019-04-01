0ea64fc383eab67ad87d1150c6dc49f016f4fb03
gerardDevelop <gerard.develop@gmail.com>
2019-04-01 15:36:37 +0700

    dsad

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ app.get('*', (req, res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 app.listen(port, () => console.log('Server running on port ' + port));
