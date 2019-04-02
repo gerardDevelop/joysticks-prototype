6f3221dc7bfd65a66c0af648ad49c0f1ad9849ae
gerardDevelop <gerard.develop@gmail.com>
2019-04-02 09:13:24 +0700

    dsd

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
-const path = require('path');
 
-app.get('*', (req, res) =>{
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
+const root = require('path').join(__dirname, 'client', 'build')
+app.use(express.static(root));
+app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+})
 
 const port = process.env.PORT || 80;
 app.listen(port, () => console.log('Server running on port ' + port));
